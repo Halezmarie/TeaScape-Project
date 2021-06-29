2e5ba259051a2413a6a2d9d571260e3881788dca
halezmarie <teacherhaleymarie@gmail.com>
2021-06-29 17:57:04 -0500

    controller logic of brand instances

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -9,19 +9,36 @@ class BrandsController < ApplicationController
   end
   
   def new
-    
+    @brand = Brand.new
   end
 
   def create
+    brand = Brand.new(brand_params)
+    @brand.user_id = session[:user_id]
+      if brand.save
+        redirect_to brand_path(@brand)
+      else
+        render :new
+      end
   end
 
   def edit
   end
 
   def update
+    if @brand.update(brand_params)
+      redirect_to brand_path(@brand)
+  else
+      render :edit
+  end
   end
 
   def destroy
   end
 
+  private
+
+  def brand_params
+      params.require(:brand).permit(:name)
+  end
 end
