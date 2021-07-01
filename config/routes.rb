44540d6bc5fa41724110880852edfa85ea984907
halezmarie <teacherhaleymarie@gmail.com>
2021-07-01 18:10:55 -0500

    Fixed routes to have nested routes that make sense

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  
-  resources :reviews
-  resources :teas
-  resources :brands, only: [:index, :new, :create]
 
-  
   root to: 'welcome#show'
 
   #signup
@@ -21,7 +16,19 @@ Rails.application.routes.draw do
   #omni
   get '/auth/:provider/callback', to: 'sessions#omniauth'
 
-  #resources
-  resources :users
- 
+  resources :reviews
+  resources :teas
+  resources :brands, only: [:index, :new, :create]
+
+  resources :users, only: [:show] 
+  resources :reviews, except: [:show, :index]
+  resources :teas 
+
+  resources :brands do # nested route
+    resources :teas, only: [:index, :new, :create]
+  end
+
+  resources :teas do # or brands? tea makes more sense 
+    resources :reviews, only: [:index, :new, :create]
+  end
 end
