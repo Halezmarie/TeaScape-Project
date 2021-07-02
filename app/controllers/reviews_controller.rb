7f0799eececfefe0c45a3861baaa28311f5efb76
halezmarie <teacherhaleymarie@gmail.com>
2021-07-02 14:38:30 -0500

    Review controller logic, starting to get it up and going

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,12 +4,24 @@ class ReviewsController < ApplicationController
   end
   
   def show
+    @review = Review.find_by_id(params[:id])
   end
     
   def new
+    if @tea = Tea.find_by_id(params[:tea_id])
+      @review = @tea.reviews.build
+    else
+      @review = Review.new
+    end
   end
   
   def create
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
   end
   
   def edit
@@ -24,6 +36,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:text, :stars)
+    params.require(:review).permit(:tea_id, :title, :text, :stars)
   end
 end
