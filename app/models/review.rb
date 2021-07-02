9e138815d09d839b0f5bbbfbe78c1ecfd1e09fec
halezmarie <teacherhaleymarie@gmail.com>
2021-07-02 14:40:44 -0500

    Edited validations in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :tea
 
-  validates :title. presence: true
-  validates :text, length { minimum: 15} # say something short at least
+  validates :title, presence: true
+  validates :text, length: { minimum: 15} # say something short at least
   # validations for stars etc,
 end
